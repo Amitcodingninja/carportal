35165e822666882f5a4b0958a185ecd1c9233b59
Amit Kumar <akumar420india@gmail.com>
2025-02-27 09:19:13 +0530

    This code extracts the JWT token, retrieves the username from it, and then forwards the request and response to the next filter.

--- src/main/java/com/carportal/config/JWTFilter.java
+++ src/main/java/com/carportal/config/JWTFilter.java
@@ -1,9 +1,11 @@
 package com.carportal.config;
 
+import com.carportal.service.JWTService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,6 +13,8 @@ import java.io.IOException;
 
 @Component
 public class JWTFilter extends OncePerRequestFilter {
+    @Autowired
+    private JWTService jwtService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -21,9 +25,11 @@ public class JWTFilter extends OncePerRequestFilter {
         System.out.println("Received Token: " + token); // Token comes to backend (console)
         if (token != null && token.startsWith("Bearer ")) {
             String jwtToken = token.substring(7);
-            System.out.println("Extracted JWT Token: " + jwtToken);
+//            System.out.println("Extracted JWT Token: " + jwtToken);
+            String username = jwtService.getUsername(jwtToken);
+            System.out.println(username);
         }
-        filterChain.doFilter(request, response);
+        filterChain.doFilter(request, response); // Passes the request and response to the next filter in the security chain.
 
     }
 }
--- src/main/java/com/carportal/service/JWTService.java
+++ src/main/java/com/carportal/service/JWTService.java
@@ -2,6 +2,7 @@ package com.carportal.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -37,6 +38,17 @@ public class JWTService {
                 .withIssuer(issuer)
                 .sign(algorithm);
     }
+
+    // Trick to remember : Junior Ronaldo is buying vegetable
+    public String getUsername(String token) {
+        DecodedJWT decodedJwt = JWT.require(algorithm)
+                .withIssuer(issuer)
+                .build()
+                .verify(token);
+        return decodedJwt.getClaim("name").asString();
+    }
+
+
 }
 // To easily remember this JWT creation flow, use this shortcut mnemonic:
 //🔑 "Claim → Expiry → Issuer → Sign" (CEIS) (By Chatgpt)
